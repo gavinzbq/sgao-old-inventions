4e924c6afb8f4fcccd3b086f83d5f50ce4401902
gavinzbq <shanyun@g.clemson.edu>
2017-08-31 16:39:30 -0400

    Add debug with pdb module

---
+++ useful_python_tricks/debug_with_pdb/example1.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+# example1.py
+
+def sum_of_squares(a, b):
+    c_squared = a**2 + b**2
+    return c_squared
+
+if __name__ == '__main__':
+    
+    import pdb; pdb.set_trace()
+
+    print(sum_of_squares(3, 4))
---
+++ useful_python_tricks/debug_with_pdb/example2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# example2.py
+
+import pdb
+
+a = 'aaa'
+b = 'bbb'
+c = 'ccc'
+
+pdb.set_trace()
+final = a + b + c
+print final
